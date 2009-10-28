c26908be219d0ced68de16ef0a24a6051fc73e4b
mogunus <mogunus@gmail.com>
2009-10-28 11:48:14 -0400

    Latest work on clause initialization.

--- clause.erl
+++ clause.erl
@@ -7,12 +7,14 @@
 
 %% 
 
-new(S,PS,Learnt) ->
+new(S,ArgBundle) ->
+
     if 
 	Learnt ->
 	    Set = sets:from_list(PS),		%remove dupes
 	    CleanedSet = sets:filter(is_false,Set)
     end,
-    
-    
 
+    OldWatches = S#solver.watches,
+    NewWatches = [|OldWatches]
+	S_next = S#solver{watches = NewWatches}
--- solver.erl
+++ solver.erl
@@ -8,44 +8,53 @@
 	 assignments}).
 
 -record(constraint_data_base,
-	{constraints,				% array(constraint): constraint records
-	 learnt_clauses,			% array(clause): learnt clauses
-	 clause_increment,			% float: amount to bump by when a clause sees activity
-	 clause_decay}).			% float: factor by which to decay clause activity
+	{constraints,				
+	 %% array(constraint): constraint records
+	 learnt_clauses,			
+	 %% array(clause): learnt clauses
+	 clause_increment,			
+	 %% float: amount to bump by when a clause sees activity
+	 clause_decay}).			
+         %% float: factor by which to decay clause activity
 
 -record(variable_ordering,
-	{activity,				% array(float): indexed by variable id. activity level.
-	 variable_increment,			% float: amount to bump by when a variable sees activity
-	 variable_decay,			% float: factor by which to decay variable activity
-	 ordering}).				% array/list(?) : variables sorted by the valyes in "activity"
+	{activity,				
+	 %% array(float): indexed by variable id. activity level.
+	 variable_increment,			
+	 %% float: amount to bump by when a variable sees activity
+	 variable_decay,			
+	 %% float: factor by which to decay variable activity
+	 ordering
+	 %% array/list(?) : variables sorted by the valyes in "activity"
+	}).
 
 %% Note! Need to figure out how to implement the variableOrder public interface.
 
 -record(propogation_rec,
-	{watches,				% array(array(constraint)): for each literal 'p', a list of
-						% constraints watching 'p'. These constraints much be inspected 
-						% when 'p' becomes true. 
-						% --------------------------------------------------------- 
-	 undos,					% array(array(constraint)): for each variable 'x', a list of
-						% constraints that need to be updated when 'x' becomes unbound
-						% in backtracking
-						% ---------------------------------------------------------
-	 queue}).				% array: propogation queue
+	{watches,				
+	 %% array(array(constraint)): for each literal 'p', a list of constraints watching
+	 %% 'p'. These constraints must be inspected when 'p' becomes true.
+	 %% -------------------------------------------------
+	 undos,					
+	 %% array(array(constraint)): for each variable 'x', a list of constraints that need to be
+	 %% updated when 'x' becomes unbound in backtracking 
+	 %% --------------------------------------------------
+	 queue}).
+	 %% array: propogation queue
 
 -record(assignment_rec,
-	{assigns,				% array(boolean): indexed by variable id, the current assignments.
-						% --------------------------------------------------------
-	 trail,					% array: assignments in chronological order; of form 
-						% {variable_id, assignment}
-						% --------------------------------------------------------
-	 trail_bounds, 				% array(int): indicies in 'trail' where the decision level changes
-						% --------------------------------------------------------
-	 reasons,				% array(constraints): indexed by variable id, the constraint that 
-						% implied the value of a variable
-						% -------------------------------------------------------
-	 levels,				% array(int): indexed by variable id, the decison level it was
-						% assigned at.
-	 root_level}).				% unsure what this one does
+	{assigns,				
+	 %% array(boolean): indexed by variable id, the current assignments.
+	 trail,					
+	 %% array: assignments in chronological order; of form {variable_id, assignment}
+ 	 trail_bounds, 				
+	 %% array(int): indicies in 'trail' where the decision level changes
+	 reasons,
+	 %% array(constraints): indexed by variable id, the constraint that implied the value of a
+	 %% variable
+	 levels,				
+	 %% array(int): indexed by variable id, the decison level it was assigned at.
+	 root_level}).				
 
 
 init_solver(S,Constraints)->
@@ -55,20 +64,13 @@ init_solver(S,Constraints)->
  
 add_constraints(Type, ArgBundles,S) ->
     AddConstraint = fun(ArgBundle,S) ->
-			    {ok, Constraint} = Type:new(S,X),
-			    OldWatches = S#solver.watches,
-			    NewWatches = 
-			    S_next = S#solver{watches = NewWatches}
-		    
-    
+			    {ok, Constraint,S} = Type:new(S,X) end,
+
     lists:foldl(AddConstraint,S,ArgBundles),
 
 
     
     
-    
-
-
 
 
 %%  21:24 < valross> mogglefrange, code:ensure_loaded(modulename)  ?
